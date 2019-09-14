0630a411a7e2f43664dad872bca49ec00d25211b
talesaraujo <talesaraujodossantos@gmail.com>
2019-09-14 20:41:47 -0300

    Added rendered dropdown select menu

--- pratica-boot-js/js/script.js
+++ pratica-boot-js/js/script.js
@@ -1,6 +1,31 @@
+/*
+    Helper function to render options inside selection forms
+*/
 var render = function(template, node) {
     node.innerHTML = template;
 }
+
+var cursosCH = ['Biblioteconomia',
+                'Ciências Sociais',
+                'História',
+                'Letras - Alemão',
+                'Letras - Espanhol',
+                'Letras - Francês',
+                'Letras - Inglês',
+                'Letras - Italiano',
+                'Letras - Português',
+                'Psicologia',
+                'Direito',
+                'Pedagogia',
+                'Arquitetura e Urbanismo',
+            ];
+
+var cursosDireito = ['Direito'];
+
+
+            
+
+
 
 var campusTemplate = `<option>Benfica</option>
                       <option>Pici</option>
@@ -8,5 +33,12 @@ var campusTemplate = `<option>Benfica</option>
                       <option>Campus de Quixadá</option>
                       <option>Campus de Sobral</option>`;
 
+var benficaCursos = `
+                    
+                    `
 
-render(campusTemplate, document.getElementById('selCampus'))
+
+
+
+
+render(campusTemplate, document.querySelector('#selCampus'));
